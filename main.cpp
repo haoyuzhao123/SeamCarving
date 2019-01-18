6ae9af8a90bd1630077ac3d0f08bc960c7cd0f03
haoyuzhao123 <thomaszhao1998@gmail.com>
2019-01-18 10:31:41 +0800

    add deletion in 2 dimension

--- main.cpp
+++ main.cpp
@@ -40,6 +40,55 @@ double min(double a, double b, double c) {
     }
 }
 
+Image transpose(const Image &img) {
+    int w = img[0].size();
+    int h = img.size();
+    Image imgnew = vector<vector<Tuple> >(w, vector<Tuple>(h, Tuple()));
+    for (int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            imgnew[j][i].x = img[i][j].x;
+            imgnew[j][i].y = img[i][j].y;
+            imgnew[j][i].z = img[i][j].z;
+        }
+    }
+    return imgnew;
+}
+
+Matrix transpose(const Matrix &mat) {
+    int w = mat[0].size();
+    int h = mat.size();
+    Matrix matnew = vector<vector<double> >(w, vector<double>(h, 0.0));
+    for (int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            matnew[j][i] = mat[i][j];
+        }
+    }
+    return matnew;
+}
+
+Image deleteline(const Image &img, const vector<int> &idx) {
+    Image newimg = img;
+    for(int i = 0; i < img.size(); i++) {
+        newimg[i].erase(newimg[i].begin() + idx[i]);
+    }
+    return newimg;
+}
+
+Matrix deleteline(const Matrix &mat, const vector<int> &idx) {
+    Matrix newmat = mat;
+    for(int i = 0; i < mat.size(); i++) {
+        newmat[i].erase(newmat[i].begin() + idx[i]);
+    }
+}
+
+Image insert(const Image &img, const vector<int> &idx) {
+    Image newimg = img;
+    for(int i = 0; i < img.size(); i++) {
+        newimg[i].insert(newimg[i].begin() + idx[i], Tuple(255,0,0));
+    }
+    return newimg;
+}
+
 Matrix rgbToGray(const Image &img) {
     int h = img.size();
     int w = img[0].size();
@@ -73,14 +122,14 @@ vector<int> seamcarving(Matrix mat) {
     // represented by indices of each row
     int h = mat.size();
     int w = mat[0].size();
-    printf("test5\n");
+    //printf("test5\n");
     Matrix dp = vector<vector<double> >(h, vector<double>(w, 0));
     for(int j = 0; j < w; j++) {
         //printf("%d\n", j);
         //printf("%lf\n", mat[0][j]);
         dp[0][j] = mat[0][j];
     }
-    printf("test6\n");
+    //printf("test6\n");
     for(int i = 1; i < h; i++) {
         for(int j = 0; j < w; j++) {
             if(j == 0) {
@@ -92,7 +141,7 @@ vector<int> seamcarving(Matrix mat) {
             }
         }
     }
-    printf("test7\n");
+    //printf("test7\n");
     double minval = 1e5;
     int idx = -1;
     for (int j = 0; j < w; j++) {
@@ -101,7 +150,7 @@ vector<int> seamcarving(Matrix mat) {
             minval = dp[h-1][j];
         }
     }
-    printf("idx: %d\n", idx);
+    //printf("idx: %d\n", idx);
     //backtracking
     vector<int> res = vector<int>(h, 0);
     res[h-1] = idx;
@@ -122,6 +171,8 @@ vector<int> seamcarving(Matrix mat) {
 }
 
 int main() {
+    // if we should mark the deleted lines
+    bool flag = true;
     //read image
     int w, h, bpp;
     unsigned char * img = stbi_load("Images/1.jpg", &w, &h, &bpp, 3);
@@ -144,24 +195,70 @@ int main() {
     }
     stbi_image_free(img);
 
-    printf("test1\n");
+    
+    //printf("test1\n");
     Matrix gray = rgbToGray(img_data);
-    printf("test2\n");
+    //printf("test2\n");
     Matrix dx = energy(gray);
-    printf("test3\n");
-    printf("%lf\n", dx[0][0]);
-    vector<int> idx = seamcarving(dx);
-    printf("test4\n");
+    //printf("test3\n");
+    //printf("%lf\n", dx[0][0]);
 
+    int delcol = 200;
+    int delrow = 100;
+    vector<vector<int> > idxcol;
+    vector<vector<int> > idxrow;
+
+    for(int i = 0; i < delcol; i++) {
+        if (i % 10 == 0)
+            printf("delete column: %d \n", i);
+        vector<int> idx = seamcarving(dx);
+        idxcol.push_back(idx);
+        img_data = deleteline(img_data, idx);
+        dx = energy(rgbToGray(img_data));
+    }
+
+    img_data = transpose(img_data);
+    dx = energy(rgbToGray(img_data));
+    printf("testtest\n");
+    for(int i = 0; i < delrow; i++) {
+        if (i % 10 == 0)
+            printf("delete row: %d \n", i);
+        vector<int> idx = seamcarving(dx);
+        idxrow.push_back(idx);
+        img_data = deleteline(img_data, idx);
+        dx = energy(rgbToGray(img_data));
+    }
+
+    img_data = transpose(img_data);
+
+    //printf("test4\n");
+    /*
     for(int i = 0; i < h; i++) {
         int j = idx[i];
         img_data[i][j].x = 255;
         img_data[i][j].y = 0;
         img_data[i][j].z = 0;
     }
+    }
+    */
+   
+    if(flag) {
+        img_data = transpose(img_data);
+        for(int i = idxrow.size()-1; i >= 0; i--) {
+            img_data = insert(img_data, idxrow[i]);
+        }
+        img_data = transpose(img_data);
+        for(int i = idxcol.size()-1; i >= 0; i--) {
+            img_data = insert(img_data, idxcol[i]);
+        }
+    }
 
 
     //write image
+    if(!flag) {
+        w = w - delcol;
+        h = h - delrow;
+    }
     unsigned char * newimg;
     newimg = new unsigned char[w * h * 3];
     int counter = 0;
@@ -174,4 +271,5 @@ int main() {
     }
     stbi_write_png("test123.png", w, h, 3, newimg, w*3);
     delete [] newimg;
+    return 0;
 }
